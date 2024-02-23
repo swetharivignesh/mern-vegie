8b2ab781ad649becaaa2cba1089d2657d2ba5c2b
swetha <swetharivignesh24@gmail.com>
2024-02-23 15:02:51 +0530

    Commit message describing the changes in the client submodule

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPjnTwv0A1fqs3eoQ5TSxDnVONNTSNTgI",
+  authDomain: "vegie-seeds.firebaseapp.com",
+  projectId: "vegie-seeds",
+  storageBucket: "vegie-seeds.appspot.com",
+  messagingSenderId: "573492119598",
+  appId: "1:573492119598:web:7f4841646b17e79f31f7bf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
